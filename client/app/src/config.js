01e3dfa92256c8aab8742c433dfd4dae471d72c3
caydey <caydey@protonmail.com>
2022-06-11 12:28:27 +0100

    env vars to use wss and change exposed port

--- client/app/src/config.js
+++ client/app/src/config.js
@@ -1,6 +1,10 @@
 // production
 let API_HOST = '/api'
 let SOCKET_HOST = `ws://${window.location.host}/socket`
+if (process.env.VUE_APP_USE_SECURE_WEBSOCKETS) { // using ssl websockets, env var loaded from './.env'
+  SOCKET_HOST = `wss://${window.location.host}/`
+}
+
 // development
 if (process.env.NODE_ENV === 'development') {
   API_HOST = 'http://localhost:3000/api'
