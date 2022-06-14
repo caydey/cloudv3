a9511d90ba73151503e69adf1ce5b504e535ba15
caydey <caydey@protonmail.com>
2022-06-14 13:13:03 +0100

    improved bytesToHuman function

--- client/app/src/helpers/bytesToHuman.js
+++ client/app/src/helpers/bytesToHuman.js
@@ -1,10 +1,12 @@
 export default function(bytes) {
-  const units = ['', 'KiB', 'MiB', 'GiB', 'TiB'];
-  if (bytes >= 1024) {
-    let e = Math.floor(Math.log(bytes) / Math.log(1024));
-    let human = (bytes / Math.pow(1024, e)).toFixed(1) + ' ' + units[e];
-
-    return human;    
+  const units = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB']
+  const BYTE = 1024 // size of a byte
+  const DECIMAL_PERCISION = 1 // number of decimal points
+  if (bytes >= BYTE) {
+    const e = Math.floor(Math.log(bytes) / Math.log(BYTE))
+    const humanUnits = bytes / Math.pow(BYTE, e)
+    const withUnit = humanUnits.toFixed(DECIMAL_PERCISION) + ' ' + units[e]
+    return withUnit
   }
-  return bytes;
+  return bytes + ' ' + units[0];
 }
