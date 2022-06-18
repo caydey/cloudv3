8a41a83a138b841fe00f23654068a676d49b065c
caydey <caydey@protonmail.com>
2022-06-18 22:22:48 +0100

    added more mimetypes and renamed old mimes

---
+++ server/app/models/mimetypes/custom.js
@@ -0,0 +1,13 @@
+module.exports = (mime) => {
+  mime.define({ 'application/x-javascript': ['js'] })
+  mime.define({ 'application/x-mswinurl': ['url'] })
+  mime.define({ 'application/x-php': ['php'] })
+  mime.define({ 'application/x-srt': ['srt'] })
+  mime.define({ 'text/x-c++': ['cpp'] })
+  mime.define({ 'text/x-csharp': ['cs'] })
+  mime.define({ 'text/x-kotlin': ['kt'] })
+  mime.define({ 'text/x-log': ['log'] })
+  mime.define({ 'text/x-python': ['py'] })
+  mime.define({ 'text/x-script': ['sh'] })
+  mime.define({ 'x-office-presentation': ['pptx'] })
+}
