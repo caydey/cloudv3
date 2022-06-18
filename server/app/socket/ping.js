a33d48359c21c1f429469d83182931de80d38275
caydey <caydey@protonmail.com>
2022-06-18 21:12:35 +0100

    removed old debug comment

--- server/app/socket/ping.js
+++ server/app/socket/ping.js
@@ -25,7 +25,6 @@ module.exports = (wss) => {
         ws.isAlive = false
         ws.ping()
       } else { // dead
-        console.log('client terminated')
         ws.terminate()
       }
     })
