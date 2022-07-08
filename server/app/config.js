44e3c94fbd61581faeade9acc099f272af4b12b5
caydey <caydey@protonmail.com>
2022-07-08 22:02:22 +0100

    added ACCESS_TOKEN to config files

--- server/app/config.js
+++ server/app/config.js
@@ -12,15 +12,23 @@ if (process.env.NODE_ENV === 'development') {
   DATA_ROOT = process.env.DATA_ROOT
 }
 
+// local access
+const LOCAL_READ_ONLY = (process.env.LOCAL_READ_ONLY === 'true')
+
 // non local access
 const NON_LOCAL_READ_ONLY = (process.env.NON_LOCAL_READ_ONLY === 'true')
+
+// access token
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN
 
 // disclose disk size
 const HIDE_DISK_SIZE = (process.env.HIDE_DISK_SIZE === 'true')
 
 module.exports = {
   STATIC_HOST,
-  NON_LOCAL_READ_ONLY,
   DATA_ROOT,
-  HIDE_DISK_SIZE
+  HIDE_DISK_SIZE,
+  LOCAL_READ_ONLY,
+  NON_LOCAL_READ_ONLY,
+  ACCESS_TOKEN
 }
