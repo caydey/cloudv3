e1acb9e57959a7f993d60f5b4e6eae94ef33630c
caydey <caydey@protonmail.com>
2022-07-18 23:00:30 +0100

    hide dot files for all except admins

--- server/app/socket/exploreWatcher.js
+++ server/app/socket/exploreWatcher.js
@@ -1,10 +1,14 @@
 const CloudPath = require('../models/CloudPath.js')
 const ExplorerHandler = require.main.require('./models/ExplorerHandler.js')
+const isAdmin = require('../middleware/isAdmin.js')
 
 const explorerHandler = new ExplorerHandler()
 
 module.exports = (wss) => {
-  wss.on('connection', (ws) => {
+  wss.on('connection', (ws, request) => {
+    // add 'admin' field to websocket object so we dont hide dot files from admins
+    ws.isAdmin = isAdmin(request.headers.cookie)
+
     ws.on('message', (event) => {
       // stop receiving updates on old path
       if (ws.aborter) {
@@ -24,6 +28,12 @@ module.exports = (wss) => {
 
 function registerWatcher (ws, cloudPath) {
   ws.aborter = explorerHandler.addExplorer(cloudPath, (response) => {
+    // hide dot files if connection is not admin
+    if (!ws.isAdmin) {
+      if (response.success && response.data.children) {
+        response.data.children = response.data.children.filter(child => !child.name.startsWith('.'))
+      }
+    }
     ws.send(JSON.stringify(response))
   })
 }
