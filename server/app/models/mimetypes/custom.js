8dc7f5c2015e11167409f87cca55096e63a02a95
caydey <caydey@protonmail.com>
2022-07-21 21:12:22 +0100

    adjusted mimetypes

--- server/app/models/mimetypes/custom.js
+++ server/app/models/mimetypes/custom.js
@@ -1,8 +1,5 @@
 module.exports = (mime) => {
-  mime.define({ 'application/x-javascript': ['js'] })
   mime.define({ 'application/x-mswinurl': ['url'] })
-  mime.define({ 'application/x-php': ['php'] })
-  mime.define({ 'application/x-srt': ['srt'] })
   mime.define({ 'text/x-c++': ['cpp'] })
   mime.define({ 'text/x-csharp': ['cs'] })
   mime.define({ 'text/x-kotlin': ['kt'] })
