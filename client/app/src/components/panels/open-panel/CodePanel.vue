6a52b07f94f1fa5de036ed006af673b996731de7
caydey <caydey@protonmail.com>
2022-07-21 21:12:06 +0100

    modularized mimetype categories

--- client/app/src/components/panels/open-panel/CodePanel.vue
+++ client/app/src/components/panels/open-panel/CodePanel.vue
@@ -1,6 +1,8 @@
 <template>
-  <span v-if="contentsLoading">LOADING</span>
-  <span v-else-if="contentsError">ERROR</span>
+  <span v-if="contentsError">ERROR</span>
+  <span v-else-if="contentsLoading">
+    <template v-if="showLoading"> LOADING </template>
+  </span>
   <highlightjs
     v-else
     class="openPanelContainer"
@@ -28,15 +30,29 @@ export default {
     return {
       contents: null,
       contentsLoading: true,
+      showLoading: false,
       contentsError: false
     }
   },
   methods: {
     updateContents() {
       this.contentsLoading = true
-      axios.get(encodeURI(this.file.location)).then((response) => {
-        this.contentsLoading = false
-        this.contents = response.data.toString();
+      // only show 'LOADING' after 1 second of loading
+      const showLoadingTimeout = setTimeout(() => {
+        this.showLoading = true
+        console.log("load");
+      }, 200)
+
+      axios.get(encodeURI(this.file.location), {
+        responseType: 'blob'
+      }).then((response) => {
+        response.data.text().then(text => { // parse Blob to String
+          this.contents = text
+          clearTimeout(showLoadingTimeout)
+          this.contentsLoading = false
+        })
+      }).catch(() => {
+        this.contentsError = true
       })
     }
   },
---
+++ client/app/src/components/panels/open-panel/MimeCategories.js
@@ -0,0 +1,38 @@
+export default {
+  code: [
+    'application/javascript',
+    'application/x-httpd-php',
+    'application/json',
+    'application/x-sh',
+    'application/xml',
+    'text/yaml',
+    'text/x-script',
+    'text/x-kotlin',
+    'text/x-python',
+    'text/x-java-source',
+    'text/x-csharp',
+    'text/x-c++',
+    'text/x-c',
+    'text/css',
+    'text/html',
+    'text/markdown',
+  ],
+  text: [
+    'text/plain',
+    'application/octet-stream',
+    'application/x-subrip'
+  ],
+  pdf: [
+    'application/pdf'
+  ],
+  video: [
+    'video/mp4',
+    'video/x-matroska',
+    'video/webm'
+  ],
+  image: [
+    'image/jpeg',
+    'image/png',
+    'image/gif'
+  ],
+}
