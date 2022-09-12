12692649cd06b46396d4af0f8230d1bf0930edd3
caydey <caydey@protonmail.com>
2022-09-12 12:48:43 +0100

    lint

--- client/app/src/helpers/iconFromMime.js
+++ client/app/src/helpers/iconFromMime.js
@@ -1,11 +1,11 @@
-export default function (mimetype) {
+export default function(mimetype) {
   if (mimetype === 'folder')
     return require('@/assets/places/folder.svg')
-  
-  let svgname = mimetype.replaceAll('/','-')
+
+  let svgname = mimetype.replaceAll('/', '-')
   try {
     return require(`@/assets/mimetypes/${svgname}.svg`)
   } catch (error) {
-    return require('@/assets/mimetypes/unknown.svg')    
+    return require('@/assets/mimetypes/unknown.svg')
   }
 }
