eb22331d8ae0fc3e0ecd23f3de57e2612f2e340c
caydey <caydey@protonmail.com>
2024-06-16 21:06:10 +0100

    server: better parsing of ip address for logs

--- server/app/helpers/getRequestInformation.ts
+++ server/app/helpers/getRequestInformation.ts
@@ -4,7 +4,8 @@ const UNKNOWN_UA = "unknown";
 
 export function getRequestIp(req: Request): string {
   if (req.headers["x-forwarded-for"])
-    return req.headers["x-forwarded-for"] as string;
+    return (req.headers["x-forwarded-for"] as string).split(", ")[0];
+
   if (req.socket.remoteAddress) return req.socket.remoteAddress;
 
   return UNKNOWN_IP;
